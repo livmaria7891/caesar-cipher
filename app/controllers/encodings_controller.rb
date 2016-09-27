f7239be11ddabe0301ff228fde99695fc4515aac
Olivia <reach.olivialegge@gmail.com>
2016-09-27 16:10:57 -0700

    added encoding

---
+++ app/controllers/encodings_controller.rb
@@ -0,0 +1,15 @@
+class EncodingsController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+
+  def index
+
+  end
+
+  def new
+  end
+
+  def show
+      #params = {'message' => 'hello world', 'shift' => 's'}
+      @message = Caesar.encode(params['shift'], params['message'])
+  end
+end
